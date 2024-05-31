2a22856c1f74b9676f9633864d55b21a0ae9320f
Toby1nik <147827372+Toby1nik@users.noreply.github.com>
2024-05-31 11:38:55 +0200

    Add clusters
    https://docs.google.com/spreadsheets/d/125Dx_TgzgDclPAQTEYquL5a0GVkND3pq62LBJDkJuSw/edit?usp=sharinga

--- main.py
+++ main.py
@@ -42,9 +42,47 @@ with open('eligible_omnix.txt', 'w') as file:
 print(f"Общий процент для OmniX: {total_percentage_omnix}% | Кол-во eligible кошельков: {len(eligible_addresses_omnix)}")
 print("Элегибл адреса OmniX записаны в файл eligible_omnix.txt")
 
-print('')
+# --------------------------------------------------------------------------------------------------------------
+
+# Открываем файл с процентами для каждого адреса (Clusters)
+with open('clusters.txt', 'r') as file:
+    lines = file.readlines()
+    address_percentages_clusters = {}
+
+    # Пропускаем первую строку с заголовком
+    header_skipped = False
+
+    for line in lines:
+        if not header_skipped:
+            header_skipped = True
+            continue
+
+        address, percentage_with_percent = line.strip().split(',')
+        # Убираем символ процента '%' и затем преобразуем строку в float
+        percentage = float(percentage_with_percent.rstrip('%'))
+        address_percentages_clusters[address.lower()] = percentage
+
+# Проверяем, какие адреса из wallets.txt есть в address_percentages_clusters
+total_percentage_clusters = 0
+eligible_addresses_clusters = []
+
+for address in wallet_addresses:
+    if address in address_percentages_clusters:
+        total_percentage_clusters += address_percentages_clusters[address]
+        eligible_addresses_clusters.append(address)
+
+# Записываем элегибл адреса в файл eligible_cluster.txt
+with open('eligible_cluster.txt', 'w') as file:
+    for address in eligible_addresses_clusters:
+        file.write(address + '\n')
+
+# Выводим результаты для Clusters
+print(f"Общий процент для Clusters: {total_percentage_clusters}% | Кол-во eligible кошельков: {len(eligible_addresses_clusters)}")
+print("Элегибл адреса Clusters записаны в файл eligible_cluster.txt")
+
+# --------------------------------------------------------------------------------------------------------------
+
 print(f'НАХЕР ОПТИМИЗАЦИЮ ПАДАЖДИ ПОКА-что...... ДУМАЕМ.gif')
-print('')
 
 # Открываем файл с процентами для каждого адреса (Polyhedra)
 df_polyhedra = pd.read_excel("polyhedra.xlsx")
@@ -71,3 +109,4 @@ print("Элегибл адреса Polyhedra записаны в файл eligib
 with open('eligible_polyhedra.txt', 'w') as file:
     for address in matching_addresses_polyhedra:
         file.write(address + '\n')
+
